27a4eea0fc4e17d2bd51396a031a43b9361de60c
Matheus <matheus.nobregamts@gmail.com>
2023-10-21 21:15:09 -0300

    fim

--- Atividade01/main.js
+++ Atividade01/main.js
@@ -1,4 +1,4 @@
-/*alert("Faça um programa que leia e valide as seguintes informações: Nome: maior que 3 caracteres; Idade: entre 0 e 150; Salário: maior que zero; Sexo: 'f' ou 'm'; Estado Civil: 's', 'c', 'v', 'd'; Dica: se sua variável é texto, o tamanho dela está armazenado em: texto.length")
+alert("Faça um programa que leia e valide as seguintes informações: Nome: maior que 3 caracteres; Idade: entre 0 e 150; Salário: maior que zero; Sexo: 'f' ou 'm'; Estado Civil: 's', 'c', 'v', 'd'; Dica: se sua variável é texto, o tamanho dela está armazenado em: texto.length")
 /*
 Faça um programa que leia e valide as seguintes informações:
 Nome: maior que 3 caracteres;
